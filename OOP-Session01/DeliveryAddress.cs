793f7961334a1a78601913653b849f08b364498d
ahmeddrabeii <ahma456ad@gmail.com>
2026-08-18 05:02:06 +0300

    Question1

---
+++ OOP-Session01/DeliveryAddress.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Session01
+{
+    //public struct DeliveryAddress
+    //{
+    //    public string City;
+    //    public string Street;
+    //}
+}
--- OOP-Session01/Program.cs
+++ OOP-Session01/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace OOP_Session01
+﻿using OOP_Session01;
+
+namespace OOPSession01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Part 01 : Theoretical Questions
+            #region Question1
+            //DeliveryAddress address1 = new DeliveryAddress();
+
+            //address1.City = "New York";
+            //address1.Street = "5th Avenue";
+
+            //DeliveryAddress address2 = address1;
+
+            //address2.Street = "Broadway";
+
+            //Console.WriteLine(address1.Street);
+            //Console.WriteLine(address2.Street);
+
+            //answer: 
+            // When a DeliveryAddress variable is copied to another variable,
+            // a copy of the value is made.Modifying the copy does not affect the original variable,
+            // because it's a struct(value type)
+            #endregion
         }
     }
 }
